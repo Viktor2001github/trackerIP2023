ba6e5f9d205f74b7b36c96b22e50efd76896f441
Viktor2001github <vvs2001.viktor@gmail.com>
2023-10-18 21:06:20 +0300

    first

---
+++ trackerIP/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	res.end("Your IP Adress:" + ` ${ipAddress}`);
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+
